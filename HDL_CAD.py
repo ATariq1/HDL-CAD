1183776dbdf447ff3dd75ba41937fd7134798435
ali <aliumar18@gmail.com>
2018-02-05 11:57:27 -0800

    completed single module

--- HDL_CAD.py
+++ HDL_CAD.py
@@ -18,15 +18,15 @@ class Module():
     def addInput(self, identifier, bus):
         self.inputs[identifier]= bus
 
-    def addOuput(self, identifier, bus):
-        self.inputs[identifier] = bus
+    def addOutput(self, identifier, bus):
+        self.outputs[identifier] = bus
 
 
-    def addAssign(self,LHS,RHS,operator):
-        self.assignments.append(LHS + operator + RHS)
+    def addAssign(self,LHS1,LHS2,operator,RHS):
+        self.assignments.append(RHS + "=" + LHS1 + operator + LHS2)
 
     def generateTitle(self):
-        Title = self.name + str(self.parameter) +"("
+        Title ="module " + self.name + " #" + str(self.parameter) +" ("
         for key in self.inputs:
             Title += key + ", "
         for key in self.outputs:
@@ -38,18 +38,35 @@ class Module():
 
         InputSet = ""
         for key in self.inputs:
-            InputSet += "input [" + str(self.inputs[key]-1) +":0]" + key +"\n"
+            InputSet += "input [" + str(self.inputs[key]-1) + ":0]" + key +"\n"
         print(InputSet)
+
+    def generateOutputs(self):
+
+            OutputSet = ""
+            for key in self.outputs:
+                OutputSet += "output [" + str(self.outputs[key] - 1) + ":0]" + key + "\n"
+            print(OutputSet)
+
+    def generateAssigns(self):
+        AssignSet = ""
+        for key in self.assignments:
+            AssignSet +="assign" +  key + "\n"
+        print(AssignSet)
+
 
 
 def main():
     And = Module("and", 1)
     And.addInput("a", 1)
     And.addInput("b", 1)
-    And.addAssign("a", "b", "&")
+    And.addOutput("c",1)
+    And.addAssign("a", "b", "&","c")
     And.generateTitle()
     And.generateInputs()
-
+    And.generateOutputs()
+    And.generateAssigns()
+    print("end module")
 
 if __name__ == '__main__':
     main()
